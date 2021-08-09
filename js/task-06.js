0a896caad66ec83d223e7d006625103490d6922c
Anatolii Bairov <tolikbairov16@gmail.com>
2021-08-09 13:10:56 +0300

    solve task-6

---
+++ js/task-06.js
@@ -0,0 +1,27 @@
+"use strict";
+const input = document.querySelector("#validation-input");
+// input.classList.add("invalid");
+
+// Изменяем цвет рамки в зависимости от введенных символов
+const changeBorderColor = () => {
+  //Соответствует ли необходимое кол-во символов введенным
+  if (Number(input.getAttribute("data-length")) === input.value.length) {
+    //если содержит класс  invalid заменяет его на valid
+    if (input.classList.contains("invalid")) {
+      input.classList.replace("invalid", "valid");
+      return;
+    }
+    input.classList.add("valid");
+    return;
+  }
+  //если содержит класс  valid заменяет его на invalid
+  if (input.classList.contains("valid")) {
+    input.classList.replace("valid", "invalid");
+    return;
+  }
+  input.classList.add("invalid");
+  console.log(input.classList);
+};
+
+// console.log(input.classList);
+input.addEventListener("change", changeBorderColor);
